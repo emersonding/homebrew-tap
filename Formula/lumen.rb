92c637818be9014b1aaf0bd9802731254f31466b
Nan Ding <nanemersonding@gmail.com>
2026-04-24 18:20:19 +0800

    lumen v2.2.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,10 +1,10 @@
 class Lumen < Formula
   desc "macOS log reviewer with color rendering and large file support"
   homepage "https://github.com/emersonding/lumen-log-viewer"
-  url "https://github.com/emersonding/lumen-log-viewer/releases/download/v2.0.0/lumen-2.0.0-arm64.tar.gz"
-  sha256 "28ce59144af91279a6abbfb8a63673a5649e77cdebb2b0fea3ca5d75c8b5781e"
+  url "https://github.com/emersonding/lumen-log-viewer/releases/download/v2.2.0/lumen-2.2.0-arm64.tar.gz"
+  sha256 "28f6a97bdbfb87f252885ca0a41d5e8dff9d0227b73362c3cf6714db214dd819"
   license "MIT"
-  version "2.0.0"
+  version "2.2.0"
 
   depends_on :macos
   depends_on macos: :sonoma
